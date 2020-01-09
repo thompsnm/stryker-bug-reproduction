2938abb696f553478b8ee93a7d3198ac6e1dd70d
Nathan Thompson <nathan.mike.thompson@gmail.com>
2020-01-09 10:59:35 -0700

    Lock Stryker version to 2.4.0

--- test/comparator.test.js
+++ test/comparator.test.js
@@ -8,8 +8,12 @@ describe('Comparator', function() {
     });
 
     describe('testedCompare', function() {
-        test('returns true when comparing two equal things', function() {
-            expect(comparator.testedCompare(1, 1)).toBe(true);
+        test('returns true when comparing two equal things', function(done) {
+            // Added timeout to make it easier to inspect .stryker-tmp before it is cleaned up
+            setTimeout(function() {
+                expect(comparator.testedCompare(1, 1)).toBe(true);
+                done();
+            }, 4000);
         });
 
         test('returns false when comparing two unequal things', function() {
